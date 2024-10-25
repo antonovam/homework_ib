6c92b05d3151354fa76fe29fdc660cfac6fbd41f
antonovam <maria.antonova@ific.uv.es>
2024-10-25 18:41:38 +0200

    read path to current folder

--- flask_server/routes.py
+++ flask_server/routes.py
@@ -4,15 +4,18 @@ import json
 from werkzeug.utils import secure_filename
 from config import Config
 
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
+
 
 def register_routes(app):
     # Ensure upload directory exists
-    os.makedirs(Config.UPLOAD_FOLDER, exist_ok=True)
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
 
     @app.route('/api/v2/get/data', methods=['GET'])
     def get_json_data():
         """Serves the JSON data from a file."""
-        file_path = os.path.join(Config.UPLOAD_FOLDER, Config.JSON_FILE)
+        file_path = os.path.join(UPLOAD_FOLDER,Config.JSON_FILE)
         if os.path.exists(file_path):
             return send_file(file_path, as_attachment=True)
         else:
@@ -25,11 +28,11 @@ def register_routes(app):
             file = request.files['file']
             if file.filename.endswith('.json'):
                 filename = secure_filename(file.filename)
-                file.save(os.path.join(Config.UPLOAD_FOLDER, filename))
+                file.save(os.path.join(UPLOAD_FOLDER, filename))
                 return jsonify({"message": "File uploaded successfully"}), 200
         elif request.is_json:
             json_data = request.get_json()
-            file_path = os.path.join(Config.UPLOAD_FOLDER, Config.JSON_FILE)
+            file_path = os.path.join(UPLOAD_FOLDER, Config.JSON_FILE)
             with open(file_path, 'w') as f:
                 json.dump(json_data, f)
             return jsonify({"message": "JSON data saved successfully"}), 200
