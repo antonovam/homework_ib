d18b5f3cb2767de54d7640ebe666d66ed54137f7
antonovam <maria.antonova@ific.uv.es>
2024-10-25 18:43:20 +0200

    minor changes in text

--- flask_server/config.py
+++ flask_server/config.py
@@ -1,5 +1,4 @@
 class Config:
     DEBUG = True
     JSON_AS_ASCII = False  # Ensure proper UTF-8 encoding for JSON responses
-    UPLOAD_FOLDER = './uploads'  # Folder to store uploaded files
     JSON_FILE = 'example.json'
